31ddddbf30f717da3b0190244eeec4fd840eac1f
bumprat <leijingyu@live.com>
2016-03-19 23:10:48 +0800

    ionic built and installed node-sass prebuild

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
+var g = require('gulp-load-plugins')();
+/*
 var gutil = require('gulp-util');
-var bower = require('bower');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+*/
+var bower = require('bower');
 var sh = require('shelljs');
+
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -15,14 +19,18 @@ gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
+    .pipe(g.sass())
+    .on('error', g.sass.logError)
+    .pipe(gulp.dest('./src/css/'))
+
+    /*
+    .pipe(g.minifyCss({
       keepSpecialComments: 0
     }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(g.rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./src/css/'))
+    */
+    
     .on('end', done);
 });
 
@@ -33,17 +41,17 @@ gulp.task('watch', function() {
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
     .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+      g.gutil.log('bower', g.gutil.colors.cyan(data.id), data.message);
     });
 });
 
 gulp.task('git-check', function(done) {
   if (!sh.which('git')) {
     console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
+      '  ' + g.gutil.colors.red('Git is not installed.'),
       '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+      '\n  Download git here:', g.gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + g.gutil.colors.cyan('gulp install') + '\' again.'
     );
     process.exit(1);
   }
